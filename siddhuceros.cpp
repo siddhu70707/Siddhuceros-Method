27bb5ae3a5d5054bcd0b76d5366c7ff6ec267035
siddhu <7376727808kushwaha@gmail.com>
2025-09-03 16:07:39 +0530

    Create siddhuceros.cpp

---
+++ siddhuceros.cpp
@@ -0,0 +1,19 @@
+// Siddhuceros Method: Efficient Square Generation
+// Discovered by Siddhu (2025)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int N = 100;
+    int squares[N+1];
+    squares[1] = 1;
+
+    for (int i = 2; i <= N; i++) {
+        squares[i] = squares[i-1] + (2*(i-1) + 1);
+    }
+
+    for (int i = 1; i <= N; i++) {
+        cout << i << "^2 = " << squares[i] << endl;
+    }
+}
